45ca8f685db33d71b830d6d664def4f41d674b58
abdixaden <72987030+abdixaden@users.noreply.github.com>
2025-04-26 12:08:24 -0400

    Generate Angle Values

--- Graph.py
+++ Graph.py
@@ -1,3 +1,6 @@
 import math
 import matplotlib.pyplot as plt
 import numpy as np  # For generating smooth angle values
+
+# Create 1000 points evenly spaced between 0 and 2π (360 degrees)
+theta = np.linspace(0, 2 * math.pi, 1000)
